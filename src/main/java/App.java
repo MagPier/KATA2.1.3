fa8e463c4f8d2a27f7d948c5f841eb48d459dbd7
Magpier <garson.ao@gmail.com>
2023-11-15 20:27:46 +0300

    Решение версия 1

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,14 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean.getMessage());
+        Cat catOne = (Cat) applicationContext.getBean("Cat");
+        Cat catTwo = (Cat) applicationContext.getBean("Cat");
+        System.out.println(bean==bean2);
+        System.out.println(catOne==catTwo);
+
+
     }
 }
